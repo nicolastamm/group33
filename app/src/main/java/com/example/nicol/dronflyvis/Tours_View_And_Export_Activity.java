e5e7e454b8a136df1ed8657279b67cafc69b023d
Artuk Kakhorov <artuk.kakhorov@uni-konstanz.de>
2018-06-21 02:10:15 +0300

    Noch ein Bug ! Input-man, die Settings array bereitet Problemme, so wie die übergeben wird ist falsch. Also die ist immer leer, und führt logischerweise zum Absturz wenn du auf die in dem lezter Act zugreifst  !
    schau dir die mall an bitte .
    
    LG
    Android Integrator.

--- app/src/main/java/com/example/nicol/dronflyvis/Tours_View_And_Export_Activity.java
+++ app/src/main/java/com/example/nicol/dronflyvis/Tours_View_And_Export_Activity.java
@@ -102,7 +102,7 @@ public class Tours_View_And_Export_Activity extends FragmentActivity implements 
             zoom = getIntent().getExtras().getFloat("com.example.nicol.dronflyvis.mapZOOM");
             mapType = getIntent().getExtras().getInt("com.example.nicol.dronflyvis.mapType");
             split =  getIntent().getExtras().getBoolean("com.example.nicol.dronflyvis.splitPoly");
-            droneFlag = getIntent().getExtras().getInt("com.example.nicol.dronflyvis.RADIO_SELECTION");
+            //droneFlag = getIntent().getExtras().getInt("com.example.nicol.dronflyvis.RADIO_SELECTION");
         }
 
 
@@ -165,7 +165,11 @@ public class Tours_View_And_Export_Activity extends FragmentActivity implements 
 
 
         if (split) {
-            Rastering raster = new Rastering(nodeList, (float) settings[2], settings[1]);
+            //Rastering raster = new Rastering(nodeList, (float) settings[2], settings[1]);
+            //Dies bereitet grade Probleme !!
+            // Problemme in dem Settings Array !
+
+            Rastering raster = new Rastering(nodeList, (float) 78.8, 100);
             TravelingSalesman tsm = new TravelingSalesman();
 
 
