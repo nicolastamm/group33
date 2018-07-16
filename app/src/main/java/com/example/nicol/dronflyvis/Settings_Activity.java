11220de7dfe18f9f7802be17362c595e1267dd2f
Heiko Dreyer <heiko.dreyer@uni-konstanz.de>
2018-07-17 01:10:43 +0200

    Merged everything, fixed some issues

--- app/src/main/java/com/example/nicol/dronflyvis/Settings_Activity.java
+++ app/src/main/java/com/example/nicol/dronflyvis/Settings_Activity.java
@@ -1,16 +1,12 @@
 package com.example.nicol.dronflyvis;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
